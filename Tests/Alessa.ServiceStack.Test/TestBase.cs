393ee012d54d5144a74d7a256c4d7f4110865371
dmaestro <dmaestro@diplomat.is>
2019-07-22 12:01:14 -0500

    Trying to add the OrmLite, but I'm saving this work to continue (probably) later... Quite later

--- Tests/Alessa.ServiceStack.Test/TestBase.cs
+++ Tests/Alessa.ServiceStack.Test/TestBase.cs
@@ -1,4 +1,3 @@
-using Funq;
 using ServiceStack;
 using ServiceStack.Data;
 using ServiceStack.OrmLite;
@@ -7,36 +6,62 @@ using Xunit;
 
 namespace Alessa.ServiceStack.Test
 {
-    public class TestBase
+    public class TestBase//: IClassFixture<TestBase>
     {
-        class AppHost : AppSelfHostBase
+        private ScriptContext ScriptContext;
+        public TestBase()
         {
-            public AppHost() : base(nameof(TestBase), typeof(TestBase).Assembly) { }
-
-            public override void Configure(Container container)
+            var context = new ScriptContext
             {
-                OrmLiteConnectionFactory factory = new OrmLiteConnectionFactory();
-                container.AddSingleton<IDbConnectionFactory>((e) =>
-                {
-                    factory.RegisterConnection("DefaultConnection", TesterBase.Constants.AlessaConnectionString, SqlServer2016Dialect.Provider);
-                    factory.RegisterConnection("TestConnection1", TesterBase.Constants.AlessaConnectionString, SqlServer2016Dialect.Provider);
-                    factory.RegisterConnection("TestConnection2", TesterBase.Constants.AlessaConnectionString, SqlServer2016Dialect.Provider);
-                    return factory;
-                });
-            }
+                ScriptMethods = { new DbScripts() },
 
+            };
+
+            context.Container.AddSingleton<IDbConnectionFactory>(() => new OrmLiteConnectionFactory(
+                 TesterBase.Constants.AlessaConnectionString, SqlServer2012Dialect.Provider));
+            this.ScriptContext = context.Init();
+
+            //context.Container.AddSingleton<IDbConnectionFactory>(() =>
+            //{
+            //    OrmLiteConnectionFactory factory = new OrmLiteConnectionFactory()
+            //    {
+            //        DialectProvider = SqlServer2016Dialect.Provider
+            //    };
+            //    factory.RegisterConnection("DefaultConnection", TesterBase.Constants.AlessaConnectionString, SqlServer2016Dialect.Provider);
+            //    factory.RegisterConnection("TestConnection1", TesterBase.Constants.AlessaConnectionString, SqlServer2016Dialect.Provider);
+            //    factory.RegisterConnection("TestConnection2", TesterBase.Constants.AlessaConnectionString, SqlServer2016Dialect.Provider);
+            //    return factory;
+            //});
+
+            this.ScriptContext = context.Init();
         }
+
+        //class AppHost : AppSelfHostBase
+        //{
+        //    public AppHost() : base(nameof(TestBase), typeof(TestBase).Assembly) { }
+
+        //    public override void Configure(Container container)
+        //    {
+        //        OrmLiteConnectionFactory factory = new OrmLiteConnectionFactory();
+        //        container.AddSingleton<IDbConnectionFactory>((e) =>
+        //        {
+        //            factory.RegisterConnection("DefaultConnection", TesterBase.Constants.AlessaConnectionString, SqlServer2016Dialect.Provider);
+        //            factory.RegisterConnection("TestConnection1", TesterBase.Constants.AlessaConnectionString, SqlServer2016Dialect.Provider);
+        //            factory.RegisterConnection("TestConnection2", TesterBase.Constants.AlessaConnectionString, SqlServer2016Dialect.Provider);
+        //            return factory;
+        //        });
+        //    }
+
+        //}
+
 
         [Fact]
         public void MyTest()
         {
-            var context = new ScriptContext
-            {
-                ScriptMethods = { new DbScriptsAsync() }
-            }.Init();
-
-            var r = context.EvaluateScript(@"{{ sql | dbSelect({ DefaultConnection }) }}
-{{ ""select Id, CompanyName, ContactName, ContactTitle, City, Country from Customer"" | assignTo: sql }}");
+            //var r = this.ScriptContext.EvaluateScript(@"Time is now: {{ now | dateFormat('HH:mm:ss') }}");
+            var r = this.ScriptContext.EvaluateScript(@"{{ sql | dbSelect({ DefaultConnection }) }}");
+            //            var r = this.ScriptContext.EvaluateScript(@"{{ sql | dbSelect({ DefaultConnection }) }}
+            //{{ ""select Id, CompanyName, ContactName, ContactTitle, City, Country from Customer"" | assignTo: sql }}");
         }
     }
 }
